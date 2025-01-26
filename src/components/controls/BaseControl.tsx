ead166010b61fdeeae95e81f69e9ba3b1bb590ae
cazdemun <cplu1509@gmail.com>
2025-01-26 15:02:10 -0500

    feat: add loading prop to base control

--- src/components/controls/BaseControl.tsx
+++ src/components/controls/BaseControl.tsx
@@ -1,20 +1,22 @@
 import React, { ReactNode } from 'react';
+import { Loader } from '@/app/ui';
 
 import styles from './BaseControl.module.scss';
 
-type BaseControlProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+type BaseProjectControlProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
   icon?: ReactNode;
   show?: BaseControlShow;
+  loading?: boolean;
 };
 
-const BaseControl: React.FC<BaseControlProps> = ({ icon: _icon, show = 'all', ...buttonProps }) => {
+const BaseControl: React.FC<BaseProjectControlProps> = ({ icon: _icon, loading, show = 'all', ...buttonProps }) => {
   const className = `${styles['control']} ${buttonProps.className ?? ''}`;
   const icon = show === 'onlyIcon' || show === 'all' ? _icon : null;
   const children = show === 'onlyText' || show === 'all' ? buttonProps.children : null;
 
   return (
     <button className={className} onClick={buttonProps.onClick} {...buttonProps}>
-      {icon} {children}
+      {loading ? <Loader /> : <>{icon} {children}</>}
     </button>
   );
 };
