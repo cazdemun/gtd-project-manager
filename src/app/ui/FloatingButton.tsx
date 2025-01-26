89df33fba22df8cbc48588245ce19ae801c40ca3
cazdemun <cplu1509@gmail.com>
2025-01-26 15:01:27 -0500

    feat: add icon to top floating button

--- src/app/ui/FloatingButton.tsx
+++ src/app/ui/FloatingButton.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AiOutlineToTop } from 'react-icons/ai';
 
 type FloatingButtonProps = {
   onClick?: () => void
@@ -18,16 +19,18 @@ const FloatingButton: React.FC<FloatingButtonProps> = () => {
         height: '50px',
         bottom: '20px',
         right: '20px',
-        padding: '10px 20px',
         backgroundColor: 'rgb(15, 15, 15)',
         color: 'white',
         border: '1px white solid',
         borderRadius: '50%',
         cursor: 'pointer',
         boxShadow: '0 2px 10px rgba(0, 0, 0, 0.2)',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      ↑
+      <AiOutlineToTop style={{ width: '25px', height: '25px' }} />
     </button>
   );
 };
