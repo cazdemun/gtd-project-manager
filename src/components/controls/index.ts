69c2aae73352a2d2c4ea168929ea348052cc2b4b
cazdemun <cplu1509@gmail.com>
2025-01-26 14:05:03 -0500

    chore: refactor index imports

---
+++ src/components/controls/index.ts
@@ -0,0 +1,3 @@
+export { default as CopyPasteControl } from './CopyPasteControl';
+export { default as DeleteProjectControl } from './DeleteProjectControl';
+export { default as EditProjectControl } from './EditProjectControl';
